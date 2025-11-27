bb36d4043d2954d23dbc36b219fc5957bcfd18f6
8pxl <56054380+8pxl@users.noreply.github.com>
2025-11-27 14:25:43 +0800

    parse skills
    ts

--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
 from sqlalchemy import Engine
 from sqlmodel import  SQLModel, Session, create_engine
+import db
 from robotevents import RobotEvents
-from teams import Teams
+from tables import Teams
 import os
 from dotenv import load_dotenv
 
@@ -9,17 +10,18 @@ from dotenv import load_dotenv
 if not load_dotenv():
     print("loading .env failed")
 
-mysqlurl = "mysql+pymysql://root:i am the state@127.0.0.1:3306/test"
+
+
+mysqlurl = f"mysql+pymysql://root:{os.environ["MYSQL_PASSWORD"]}@127.0.0.1:3306/test"
 engine = create_engine(mysqlurl, echo=True)
 SQLModel.metadata.create_all(engine)
 
-def add_teams(engine: Engine, team: Teams):
-    session = Session(engine)
-    exisisting = session.get(Teams, team.id)
-    if not exisisting:
-        session.add(team)
-        session.commit()
-
 robotevents = RobotEvents(os.environ["ROBOTEVENTS_AUTH_TOKEN"])
-team = robotevents.get_all_teams()
-add_teams(engine, team)
+# team = robotevents.get_all_teams()
+# print(robotevents.get_qualifications(109693))
+teams = (robotevents.parse_skills())
+for team in teams:
+    db.add_team(engine,team)
+    print("adding ", team)
+
+# add_teams(engine, team)
--- src/robotevents.py
+++ src/robotevents.py
@@ -16,12 +16,13 @@ class RobotEvents:
         self.token = token
         self.base = "https://www.robotevents.com/api/v2"
         self.season = 197
+        # self.season = 190
         self.header = {
             "Authorization": f"Bearer {token}"
         }
 
-    def request(self, path: str) -> Any | None:  # pyright: ignore[reportExplicitAny, reportAny]
-        if path[0] != "/":
+    def request(self, path: str) -> Any | None:  # pyright: ignore[reportExplicitAny]
+        if not path.startswith("/"):
             self.logger.exception("path needs to start with a blackslash")
             return (None)
         url = self.base + path
@@ -49,21 +50,43 @@ class RobotEvents:
         return highest
 
 
-    def create_team(self, robotevents_id: int, score: tuple[int, int, int]):
+    def create_team(self, robotevents_id: int, rank: int, score:int, programming: int, driver: int):
         res = self.request(f"/teams/{robotevents_id}")
         if not res:
             return None
         team = Teams(
-            id=res['id'],  # pyright: ignore[reportAny] 
-            number=res['number'],  # pyright: ignore[reportAny]
+            id = res['id'],  # pyright: ignore[reportAny] 
+            number = res['number'],  # pyright: ignore[reportAny]
             organization = res['organization'],   # pyright: ignore[reportAny]
-            country= res['location']['country'],  # pyright: ignore[reportAny]
+            country = res['location']['country'],  # pyright: ignore[reportAny]
             registered = res['registered'],  # pyright: ignore[reportAny]
             grade = res['grade'],  # pyright: ignore[reportAny]
-            qualification =  self.get_qualifications(res['id']),  # pyright: ignore[reportAny]
-            score = score[0],
-            programming = score[1],
-            driver = score[2],
+            qualification = self.get_qualifications(res['id']),  # pyright: ignore[reportAny]
+            world_rank = rank,
+            score = score,
+            programming = programming,
+            driver = driver
         )
         return team
 
+    def parse_skills(self) ->list[Teams]:
+        url = "https://www.robotevents.com/api/seasons/197/skills?post_season=0&grade_level=High%20School"
+        res = requests.get(url)
+        try:
+            res.raise_for_status()
+        except requests.RequestException as exc:
+            self.logger.exception("API request failed: %s %s", url, exc)
+        res = res.json()
+        # print("request went through!")
+        # print(res)
+        teams = []
+        for team in res:
+            id: int = team["team"]["id"]
+            rank: int = team["rank"]
+            score =team["scores"]["score"]
+            programming = team["scores"]["programming"]
+            driver = team["scores"]["driver"]
+            teams.append(self.create_team(id, rank, score, programming, driver))
+            if (len(teams) >= 5):
+                break
+        return teams
