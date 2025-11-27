aa014e6d33d733d3423b219c37f0a1e1d8ebd742
8pxl <56054380+8pxl@users.noreply.github.com>
2025-11-27 16:30:12 +0800

    add qualify, change add to upsert

--- src/db.py
+++ src/db.py
@@ -1,12 +1,31 @@
 from sqlalchemy import Engine
 from sqlmodel import Session
-from tables import Teams
+from tables import Qualification, Teams
 
 
-def add_team(engine: Engine, team: Teams):
-    session = Session(engine)
-    exisisting = session.get(Teams, team.id)
-    if not exisisting:
-        session.add(team)
+def upsert(engine: Engine, team: Teams):
+    with Session(engine) as session:
+        _ = session.merge(team)
         session.commit()
 
+def qualify(engine: Engine, id: int):
+    with Session(engine) as session:
+        existing = session.get(Teams,id)
+        if not existing:
+            print(f"team {id} doesnt exist yet!")
+            return
+        else:
+            existing.qualification = Qualification.WORLD
+        session.commit();
+        
+
+    # existing = session.get(Teams, team.id)
+    #     if not existing:
+    #         session.add(team)
+    #     else:
+    #         existing.score = team.score
+    #         existing.driver = team.driver
+    #         existing.rank = team.world_rank
+    #         existing.programming = team.programming
+    #         existing.qualification = team.qualification
+    #     session.commit()
