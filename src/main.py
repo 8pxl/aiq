2f19fb30d63b4c5b9ef4ab74606a4a67a96afe20
8pxl <56054380+8pxl@users.noreply.github.com>
2025-11-27 18:33:11 +0800

    timing logic for running slow update

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
+from datetime import datetime, timedelta
 from sqlalchemy import Engine
 from sqlmodel import  SQLModel, Session, create_engine
 import db
 from robotevents import RobotEvents
-from tables import Teams
+from tables import Teams, Qualifications
 import os
 from dotenv import load_dotenv
 
@@ -14,15 +15,17 @@ mysqlurl = f"mysql+pymysql://root:{os.environ["MYSQL_PASSWORD"]}@127.0.0.1:3306/
 engine = create_engine(mysqlurl, echo=True)
 SQLModel.metadata.create_all(engine)
 
+print("fast step")
 robotevents = RobotEvents(os.environ["ROBOTEVENTS_AUTH_TOKEN"])
 teams = (robotevents.parse_skills())
 for team in teams:
     db.upsert(engine,team)
     print("adding ", team)
 
-# worlds_registered = robotevents.get_worlds_teams()
-# if worlds_registered:
-#     for team in worlds_registered:
-#         db.qualify(engine, team)
-
-# add_teams(engine, team)
+if db.get_last_qualification_update(engine) - datetime.now() > timedelta(weeks=1):
+    all_teams = db.get_all_teams(engine)
+    qualifications  = robotevents.create_qualifications_full(all_teams)
+    print("updating qualifications!")
+    for q in qualifications:
+        db.upsert(engine, q)
+# self.create_qualifications_worlds_fast()
