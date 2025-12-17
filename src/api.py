e74c730dc193c6579657af10d4a87906ac5f29d4
8pxl <56054380+8pxl@users.noreply.github.com>
2025-12-17 15:24:36 +0800

    add middleware support

--- src/api.py
+++ src/api.py
@@ -1,10 +1,36 @@
-from fastapi import APIRouter, Depends
-from sqlmodel import Session
+from fastapi import APIRouter, Depends, FastAPI
+from sqlmodel import Session, select
 
 import db
+from tables import Teams
 
-router = APIRouter()
+from fastapi.middleware.cors import CORSMiddleware
 
-@router.get("/teams")
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+@app.get("/teams")
 def get_teams(session: Session = Depends(db.get_session)):
     return {"teams": db.get_all_teams(session)}
+
+@app.get("/teams")
+def get_leaderboard(
+    ms: bool = False,
+    region: str | None = None,
+    session: Session = Depends(db.get_session)):
+    query = select(Teams)
+    if ms:
+        query = query.where(Teams.grade == "Middle School")
+    return {
+        "teams": session.exec(query)
+    }
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
