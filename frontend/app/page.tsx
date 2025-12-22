a1aec9385ca87765778933474c82edfbdb61140f
8pxl <56054380+8pxl@users.noreply.github.com>
2025-12-22 15:13:26 +0800

    rename /dev to /dash

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -4,7 +4,7 @@ import SignIn from "./components/auth/sign-in";
 export default function Home() {
   return (
     <main>
-      <Link href="/dev" className="" >
+      <Link href="/dash" className="" >
         open dashboard
       </Link>
     </main>
