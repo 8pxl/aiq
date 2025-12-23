56b0f6bb2049c810092368fda54a18b936a06a21
8pxl <56054380+8pxl@users.noreply.github.com>
2025-12-23 17:42:10 +0800

    add shared refresh state

--- frontend/app/dash/dashboard.tsx
+++ frontend/app/dash/dashboard.tsx
@@ -1,7 +1,9 @@
+"use client"
 import Link from "next/link"
 import Quals, { QualsInput } from "./quals"
 import { authClient } from "@/lib/auth-client"
 import QualsDisplay from "./quals";
+import { useState } from "react";
 
 function Header() {
   async function handleSignOut(e: MouseEvent<HTMLAnchorElement>) {
@@ -26,15 +28,16 @@ function Welcome() {
   )
 }
 export default function Dashboard() {
+  const [refresh, setRefresh] = useState(0)
   return (
     <div className="flex flex-col m-3 h-[calc(100vh-24px)]">
       <Header />
       < Welcome />
       <div className="mt-2.5 grid grid-rows-3 grid-cols-3 border-1 border-white divide-x divide-y divide-white gap-0 overflow-auto">
-        <QualsDisplay />
+        <QualsDisplay refresh={refresh} />
         <div></div>
         <div></div>
-        <QualsInput />
+        <QualsInput refresh={refresh} setRefresh={setRefresh} />
         <div></div>
         <div></div>
       </div>
