235417a851feaf90aa4978d770e08b4549239167
8pxl <56054380+8pxl@users.noreply.github.com>
2026-02-16 18:46:01 -0500

    gen sdk

--- frontend/lib/client/client.gen.ts
+++ frontend/lib/client/client.gen.ts
@@ -13,4 +13,4 @@ import type { ClientOptions as ClientOptions2 } from './types.gen';
  */
 export type CreateClientConfig<T extends ClientOptions = ClientOptions2> = (override?: Config<ClientOptions & T>) => Config<Required<ClientOptions> & T>;
 
-export const client = createClient(createConfig<ClientOptions2>({ baseUrl: 'http://128.61.104.81:8000' }));
+export const client = createClient(createConfig<ClientOptions2>({ baseUrl: 'https://subtetanic-rhapsodically-roxana.ngrok-free.dev' }));
--- frontend/lib/client/sdk.gen.ts
+++ frontend/lib/client/sdk.gen.ts
@@ -2,7 +2,7 @@
 
 import type { Client, Options as Options2, TDataShape } from './client';
 import { client } from './client.gen';
-import type { GetLastSlowLastSlowGetData, GetLastSlowLastSlowGetErrors, GetLastSlowLastSlowGetResponses, GetLeaderboardLbGetData, GetLeaderboardLbGetErrors, GetLeaderboardLbGetResponses, GetQualificationsQualificationsGetData, GetQualificationsQualificationsGetResponses, GetRegionsRegionsGetData, GetRegionsRegionsGetResponses, GetTeamsTeamsGetData, GetTeamsTeamsGetErrors, GetTeamsTeamsGetResponses, PutQualificationsQualificationsPutData, PutQualificationsQualificationsPutErrors, PutQualificationsQualificationsPutResponses, RootGetData, RootGetResponses } from './types.gen';
+import type { GetLeaderboardLbGetData, GetLeaderboardLbGetErrors, GetLeaderboardLbGetResponses, GetQualificationsQualificationsGetData, GetQualificationsQualificationsGetResponses, GetRegionsRegionsGetData, GetRegionsRegionsGetResponses, RootGetData, RootGetResponses } from './types.gen';
 
 export type Options<TData extends TDataShape = TDataShape, ThrowOnError extends boolean = boolean> = Options2<TData, ThrowOnError> & {
     /**
@@ -19,11 +19,6 @@ export type Options<TData extends TDataShape = TDataShape, ThrowOnError extends 
 };
 
 /**
- * Get Teams
- */
-export const getTeamsTeamsGet = <ThrowOnError extends boolean = false>(options: Options<GetTeamsTeamsGetData, ThrowOnError>) => (options.client ?? client).get<GetTeamsTeamsGetResponses, GetTeamsTeamsGetErrors, ThrowOnError>({ url: '/teams', ...options });
-
-/**
  * Get Regions
  */
 export const getRegionsRegionsGet = <ThrowOnError extends boolean = false>(options?: Options<GetRegionsRegionsGetData, ThrowOnError>) => (options?.client ?? client).get<GetRegionsRegionsGetResponses, unknown, ThrowOnError>({ url: '/regions', ...options });
@@ -37,16 +32,6 @@ export const getLeaderboardLbGet = <ThrowOnError extends boolean = false>(option
  * Get Qualifications
  */
 export const getQualificationsQualificationsGet = <ThrowOnError extends boolean = false>(options?: Options<GetQualificationsQualificationsGetData, ThrowOnError>) => (options?.client ?? client).get<GetQualificationsQualificationsGetResponses, unknown, ThrowOnError>({ url: '/qualifications', ...options });
-
-/**
- * Put Qualifications
- */
-export const putQualificationsQualificationsPut = <ThrowOnError extends boolean = false>(options: Options<PutQualificationsQualificationsPutData, ThrowOnError>) => (options.client ?? client).put<PutQualificationsQualificationsPutResponses, PutQualificationsQualificationsPutErrors, ThrowOnError>({ url: '/qualifications', ...options });
-
-/**
- * Get Last Slow
- */
-export const getLastSlowLastSlowGet = <ThrowOnError extends boolean = false>(options: Options<GetLastSlowLastSlowGetData, ThrowOnError>) => (options.client ?? client).get<GetLastSlowLastSlowGetResponses, GetLastSlowLastSlowGetErrors, ThrowOnError>({ url: '/lastSlow', ...options });
 
 /**
  * Root
